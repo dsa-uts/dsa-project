d0cd9ff50a7b63053e428488e5a0a5c59566dbc5
Takuya Mizokami <37259755+takoyaki65@users.noreply.github.com>
2025-10-04 15:47:02 +0900

    Revert "1st nuke"

--- dsa-backend/handler/problem/constants.go
+++
@@ -1,8 +0,0 @@
-package problem
-
-const (
-	UPLOAD_DIR     = "upload"
-	RESOURCE_DIR   = "upload/resource"
-	VALIDATION_DIR = "upload/validation"
-	GRADING_DIR    = "upload/grading"
-)
--- dsa-judgeserver/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-go install github.com/air-verse/air@latest
-
-air -c .air.toml
