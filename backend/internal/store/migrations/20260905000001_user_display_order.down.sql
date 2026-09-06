4280ae235631f63111c59195a09d5d56f2cdfee2
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2026-09-06 20:25:23 +0900

    shrink migration files

--- backend/internal/store/migrations/20260905000001_user_display_order.down.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE user_accounts DROP COLUMN display_order;
--- backend/internal/store/migrations/20260906000001_deferrable_user_order.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user_accounts DROP CONSTRAINT user_accounts_display_order_key;
-CREATE UNIQUE INDEX user_accounts_display_order_idx ON user_accounts (display_order);
